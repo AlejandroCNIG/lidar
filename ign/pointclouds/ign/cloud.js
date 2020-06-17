8f8e29b019583ef96f8f0df33e8c28090cf266fd
AlejandroCNIG <alejandro.garcia@cnig.es>
2020-06-17 16:20:38 +0100

    added ign pointcloud

---
+++ ign/pointclouds/ign/cloud.js
@@ -0,0 +1,30 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 1856556,
+    "boundingBox": {
+        "lx": 439000.0,
+        "ly": 4477000.0,
+        "lz": 650.5500000000001,
+        "ux": 439999.9900000002,
+        "uy": 4477999.99,
+        "uz": 1650.5400000002237
+    },
+    "tightBoundingBox": {
+        "lx": 439000.0,
+        "ly": 4477000.0,
+        "lz": 650.5500000000001,
+        "ux": 439999.99,
+        "uy": 4477999.99,
+        "uz": 758.58
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED",
+        "INTENSITY"
+    ],
+    "spacing": 8.660167694091797,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
